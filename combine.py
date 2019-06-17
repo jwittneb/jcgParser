c05ba3e46eb55063f699d547ee21fda16a80bb98
John <jwittneb@uwaterloo.ca>
2019-06-17 18:29:10 -0400

    Combining several JCG stats together edits

--- combine.py
+++ combine.py
@@ -1,31 +1,44 @@
 import ast
 from archsMod import archs
 
-matrixfile = open("matrix.txt", "r")
+matrixfile = open("archmatrix.txt", "r")
 
 f1 = matrixfile.readlines()
 comMatrix = []
 
+# combines two rows of tuple with the same length
 def combineRows(row1,row2):
+    if (len(row1) != len(row2)):
+        print "ERROR: Attempted to combine rows of different length"
+        return -1
     for j in range(len(row1)):
         row1[j][0] += row2[j][0]
         row1[j][1] += row2[j][1]
     return row1
 
-for i in range(len(archs)):
-    comMatrix.append(0)
+def printMatrix(imat):
+    for i in range(len(imat)):
+        print imat[i]
 
-i=0
-for x in f1:
-    if (comMatrix[i] == 0):
-        comMatrix[i] = ast.literal_eval(x)
-    else:
-        comMatrix[i] = combineRows(comMatrix[i], ast.literal_eval(x))
-    if (i == 7):
-        i = 0
-    else:
-        i += 1
+def initMat(imat):
+    for i in range(len(archs)):
+        comMatrix.append(0)
 
-for i in range(len(comMatrix)):
-    print comMatrix[i]
+def collapseRows(iMatrix):
+    i=0
+    for x in f1:
+        if (iMatrix[i] == 0):
+            iMatrix[i] = ast.literal_eval(x)
+        else:
+            iMatrix[i] = combineRows(iMatrix[i], ast.literal_eval(x))
+        if (i == len(archs)-1):
+            i = 0
+        else:
+            i += 1
+    return iMatrix
 
+#def printInfo(iMatrix):
+
+initMat(comMatrix)
+comMatrix = collapseRows(comMatrix)
+printMatrix(comMatrix)
