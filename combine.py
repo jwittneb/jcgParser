2d5d5fac4559f676b5a83011ee0b659031190b5a
John <jwittneb@uwaterloo.ca>
2019-06-17 20:00:19 -0400

    more changes to combining several JCG instances

--- combine.py
+++ combine.py
@@ -1,10 +1,15 @@
 import ast
 from archsMod import archs
+from fractions import Fraction
 
 matrixfile = open("archmatrix.txt", "r")
 
+numArchs = len(archs)
 f1 = matrixfile.readlines()
 comMatrix = []
+
+def sortSecond(val):
+    return val[1]
 
 # combines two rows of tuple with the same length
 def combineRows(row1,row2):
@@ -37,8 +42,23 @@ def collapseRows(iMatrix):
             i += 1
     return iMatrix
 
-#def printInfo(iMatrix):
+#TODO: this is similar to what is done in parseArch, generalize+combine
+def printInfo(iMatrix):
+    winrates = []
+    for i in range(numArchs):
+        print str(archs[i][0]) + ": " + str(iMatrix[i])
+        winrates.append([archs[i][0],0])
+    for i in range(numArchs):
+        classWins = 0
+        classGames = 0
+        for j in range(numArchs):
+            classWins += iMatrix[i][j][0]
+            classGames += iMatrix[i][j][1]
+        winrates[i][1] = Fraction(classWins,classGames)
+    winrates.sort(key = sortSecond)
+    for i in range(len(winrates)):
+        print (winrates[i][0] + " = " + str(round(float(winrates[i][1]),4)))
 
 initMat(comMatrix)
 comMatrix = collapseRows(comMatrix)
-printMatrix(comMatrix)
+printInfo(comMatrix)
